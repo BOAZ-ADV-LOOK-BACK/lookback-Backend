143fe2a63ca3a7b91fe7cd73a632c7dd33cc55ee
CHOI JEE HYUK <akfktl328@kyongi.ac.kr>
2024-09-18 23:24:16 +0900

    [KAN-46] token.py 'flush=True' 추가

--- app/api/v1/endpoints/token.py
+++ app/api/v1/endpoints/token.py
@@ -12,7 +12,7 @@ class TokenData(BaseModel):
 @router.post("/save-token")
 async def save_token(token_data: TokenData):
     # 액세스 토큰 출력 (DB 저장이나 추가 작업을 여기에 작성)
-    print(f"Received Access Token: {token_data.access_token}")
+    print(f"Received Access Token: {token_data.access_token}", flush=True)
 
     # 여기에 액세스 토큰 저장 로직 추가 (예: 데이터베이스에 저장)
     # 예시: db.save_token(token_data.access_token)
