ced059144c192367a553af961b941ce047c7918e
YEERRin <ryeon0107@gmail.com>
2024-10-07 20:49:38 +0900

    [KAN 47] SQL 스키마 수정

--- app/db/database.py
+++ app/db/database.py
@@ -2,7 +2,14 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "mysql://ubuntu:pw@localhost:3306/lookback"
+import os 
+from dotenv import load_dotenv
+
+load_dotenv()
+
+DB_PWD = os.environ.get("DB_PWD")
+
+SQLALCHEMY_DATABASE_URL = f"mysql://ubuntu:{DB_PWD}@localhost:3306/lookback"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
