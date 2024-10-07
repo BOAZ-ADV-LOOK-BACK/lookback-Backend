f8a747d5b3fe6297637df236c2b770f8ef2a0c22
YEERRin <ryeon0107@gmail.com>
2024-10-07 20:00:03 +0900

    [KAN47] 프론트엔드에서 액세스 토큰 받아오고 전처리 후 DB 적재

--- app/api/v1/endpoints/token.py
+++ app/api/v1/endpoints/token.py
@@ -1,23 +1,40 @@
-# app/api/v1/endpoints/token.py
-
+from fastapi.responses import RedirectResponse
 from fastapi import APIRouter, HTTPException, Depends
 from pydantic import BaseModel
 import logging
+
+import json
 logging.basicConfig(level=logging.INFO)
 
 router = APIRouter()
 
+
+REDIRECT_URI = 'https://api.look-back.site/api/v1/save-token'
+
 # 요청 본문에 필요한 데이터 스키마 정의
 class TokenData(BaseModel):
     access_token: str
+
+
+def credentials_to_dict(credentials):
+    return {
+        'token': credentials.access_token,
+#        'refresh_token': credentials.refresh_token,
+        #'token_uri': credentials.token_uri,
+        #'client_id': credentials.client_id,
+        #'client_secret': credentials.client_secret,
+        #'scopes': credentials.scopes
+    }
 
 @router.post("/save-token")
 async def save_token(token_data: TokenData):
     # 액세스 토큰 출력 (DB 저장이나 추가 작업을 여기에 작성)
     print(f"Received Access Token: {token_data.access_token}", flush=True)
     logging.info(f"Received Access Token: {token_data.access_token}")
+    
+    credentials = token_data
 
-    # 여기에 액세스 토큰 저장 로직 추가 (예: 데이터베이스에 저장)
-    # 예시: db.save_token(token_data.access_token)
+    response = RedirectResponse(url='/preprocess/calendar')
+    response.set_cookie(key='credentials', value=json.dumps(credentials_to_dict(credentials)), httponly=True)
 
     return {"message": "Token received and processed"}
