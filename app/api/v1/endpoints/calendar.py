9af712877c73aa2ac8cd82fd337c35d0efcb4255
enginnerK <gyeo009@gmail.com>
2024-11-24 20:38:52 +0900

    각 로직 분리 및 파일 구조, 함수 호출 구조 생성

---
+++ app/api/v1/endpoints/calendar.py
@@ -0,0 +1,36 @@
+# 캘린더 관련 API 모음
+from fastapi import APIRouter, HTTPException, Depends
+from pydantic import BaseModel
+from app.api.v1.endpoints import login, users, google, calendar, data_preprocessing
+
+
+import httpx
+import json
+
+router = APIRouter()
+
+
+
+### 캘린더 API
+# 1. 캘린더 데이터 요청
+# 2. 전처리
+# 3. dynamodb에 비동기로 삽입
+# 4. 프론트로 데이터 return
+@router.get("/dashboard-data")
+async def get_dashboard_data(code):
+    # token_info 예시는 print 후 확인 요망
+    token_info = google.get_access_token(code)
+
+
+    # 1. 캘린더 데이터 요청
+    calendar_data_origin = google.get_calendar_data(code)
+
+    # 2. 전처리
+    # data_preprocessing.py 내부 함수 참고
+
+    # 3. dynamodb 비동기 삽입
+    # data_preprocessing.py 내부 함수 참고
+    
+    # 4. 프론트로 리턴
+    # 2번에서 전처리 한 결과 리턴해주면 됨
+    # 향후 프론트에서 이 데이터 받아서 알아서 잘 각 시각화 component에 잘 매핑
--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from app.api.v1.endpoints.get_authorization import router as auth_router
 from app.api.v1.endpoints.data_preprocessing import router as preprocess_router
-from app.api.v1.endpoints import login, users, google
+from app.api.v1.endpoints import login, users, google, calendar
 import os
 
 app = FastAPI()
@@ -13,6 +13,9 @@ app.include_router(preprocess_router, prefix='/preprocess')
 app.include_router(login.router, prefix="/api/v1")
 app.include_router(users.router, prefix="/api/v1")
 app.include_router(google.router, prefix="/api/v1")
+app.include_router(calendar.router, prefix="/api/v1")
+
+
 
 
 # app/main.py
