4ce013788850a5c2d3cecc281c8e49d80f47ef12
enginnerK <gyeo009@gmail.com>
2024-11-24 20:59:24 +0900

    비동기 함수 await 키워드 추가

--- app/api/v1/endpoints/google.py
+++ app/api/v1/endpoints/google.py
@@ -10,35 +10,37 @@ router = APIRouter()
 # parameter - authorization code
 # return - json type의 token_info 를 반환함
 async def get_access_token(code):
-        token_url = "https://oauth2.googleapis.com/token"
-        token_info = ""
-        # 나중에 Google
-        with open("client_secret_639048076528-0mqbo91cf5t0fq5604u0tblqnaka8thp.apps.googleusercontent.com.json", "r") as f:
-            client_config = json.load(f)["web"]
+    token_url = "https://oauth2.googleapis.com/token"
+    token_info = ""
 
-        token_data = {
-            "code": code,
-            "client_id": client_config["client_id"],
-            "client_secret": client_config["client_secret"],
-            "redirect_uri": "postmessage",
-            "grant_type": "authorization_code"
-        }
+    # 클라이언트 구성 읽기
+    with open("client_secret_639048076528-0mqbo91cf5t0fq5604u0tblqnaka8thp.apps.googleusercontent.com.json", "r") as f:
+        client_config = json.load(f)["web"]
 
-        async with httpx.AsyncClient() as client:
-            token_response = await client.post(token_url, data=token_data)
-            token_response.raise_for_status()
-            token_info = token_response.json()
-        
-        return token_info
+    token_data = {
+        "code": code,
+        "client_id": client_config["client_id"],
+        "client_secret": client_config["client_secret"],
+        "redirect_uri": "postmessage",
+        "grant_type": "authorization_code",
+    }
+
+    # 비동기 컨텍스트 매니저로 httpx.AsyncClient 사용
+    async with httpx.AsyncClient() as client:
+        token_response = await client.post(token_url, data=token_data)
+        token_response.raise_for_status()
+        token_info = token_response.json()
+
+    return token_info
 
 
-### 캘린더 데이터 통째로 받아오는 함수
-# parameter - authorization code
-# return - json type의 calendar data
-async def get_calendar_data(code):
+# ### 캘린더 데이터 통째로 받아오는 함수
+# # parameter - authorization code
+# # return - json type의 calendar data
+# async def get_calendar_data(code):
     
-    # access token 받아오기
-    token_info = get_access_token(code)
+#     # access token 받아오기
+#     token_info = get_access_token(code)
 
-    # calendar data 요청
+#     # calendar data 요청
 
