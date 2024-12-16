27360deee2ee3a303025df929a47c79b21fb04e9
CHOI JEE HYUK <akfktl328@kyongi.ac.kr>
2024-12-16 16:46:20 +0900

    main.py에 prefix 추가

--- app/main.py
+++ app/main.py
@@ -1,3 +1,4 @@
+# main.py
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from app.api.v1.endpoints import login, users, google, calendar
@@ -8,24 +9,20 @@ app = FastAPI()
 
 os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
 
+# API 라우터 등록 - 각각의 prefix를 명확하게 구분
 app.include_router(login.router, prefix="/api/v1")
-app.include_router(users.router, prefix="/api/v1")
-app.include_router(google.router, prefix="/api/v1")
-app.include_router(calendar.router, prefix="/api/v1")
+app.include_router(users.router, prefix="/api/v1/users")
+app.include_router(google.router, prefix="/api/v1/google")
+app.include_router(calendar.router, prefix="/api/v1/calendar")  # calendar는 /calendar prefix 사용
 
-
-
-
-# app/main.py
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # or specify the domain(s) you want to allow
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
-# 나머지 FastAPI 애플리케이션 설정
 @app.get("/")
 async def root():
     return {"message": "Welcome to LookBack API"}
